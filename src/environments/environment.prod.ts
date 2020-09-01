d2fb333e192819a6bf7621d6c43c9884a4c496f8
Mohamed Yousuf <mohamed.yousuf@intellectdesign.com>
2020-09-01 23:57:46 +0530

    modified environment file for firebase V1.4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAn_WQHFfM9fEq4ZPkxDHT0PheSRSKBUh0",
+    authDomain: "fb-buy-and-sell.firebaseapp.com",
+    databaseURL: "https://fb-buy-and-sell.firebaseio.com",
+    projectId: "fb-buy-and-sell",
+    storageBucket: "fb-buy-and-sell.appspot.com",
+    messagingSenderId: "344743767504",
+    appId: "1:344743767504:web:5627664f697ea019ed9dd5"
+  }
 };
